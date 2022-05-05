6cc2543c6a63fbb7ce91fc78434545be35bbf49d
AlekseiZaitsevWeb <mysbor24@gmail.com>
2022-05-05 22:29:54 +0300

    Added slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,11 @@
 import './index.css';
+import ItcSimpleSlider from "../components/simple-adaptive-slider";
+
+// инициализация слайдера
+const slider = new ItcSimpleSlider('.slider', {
+  loop: true,
+  autoplay: true,
+  interval: 5000,
+  swipe: false
+});
+
